d20a073de6f9fcd5dfae04d30ba47e26262ccc43
Jones-Davy <93669296+Jones-Davy@users.noreply.github.com>
2022-10-07 18:45:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,11 +11,7 @@
   sr.reveal(`.new-search-form`, {delay: 500})
   sr.reveal(`.popular__card`, {origin: 'bottom', delay: 300, interval: 100})
   sr.reveal(`.aside-block`, {origin: 'left', delay: 300, distance: '40px'})
-  if (document.documentElement.clientWidth > 768) {
-  sr.reveal( `.footer__data`, {delay: 300, origin: 'right'} )
-  sr.reveal( `.footer__social`, {delay: 500, origin: 'bottom', interval: 100} )
+  sr.reveal( `.footer__content`, {delay: 300, origin: 'right'} )
 
-
-  }
   
 
