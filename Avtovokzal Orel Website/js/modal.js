905d4b8a027b775f0c03dc74f5eb0ec4ca1e8c91
Jones-Davy <93669296+Jones-Davy@users.noreply.github.com>
2022-10-07 18:25:02 +0300

    Add files via upload

---
+++ Avtovokzal Orel Website/js/modal.js
@@ -0,0 +1,45 @@
+const navLink = document.querySelector('.nav__link')
+const overlayRegistration = document.querySelector('.overlay__registration')
+const overlayEnter = document.querySelector('.overlay__enter')
+const form = document.querySelector('.new-search-form')
+const enterLink = document.querySelector('.enter__link')
+const regLink = document.querySelector('.reg__link')
+const modalTitle = document.querySelector('.modal__title')
+const inputName = document.querySelector('.modal__label-name')
+
+    navLink.addEventListener('click', () => {
+        overlayRegistration.style.display = 'flex'
+        document.body.style.overflow = 'hidden';
+        form.style.display = 'none'
+    })
+
+overlayRegistration.addEventListener('click', event => {
+    const target = event.target
+    if (target === overlayRegistration || target.closest('.modal__close')) {
+        overlayRegistration.style.display = 'none'
+        document.body.style.overflow = 'visible';
+        form.style.display = 'table'
+    }
+})
+
+enterLink.addEventListener('click', () =>{
+    overlayRegistration.style.display = 'none'
+    overlayEnter.style.display = 'flex'
+
+})
+
+regLink.addEventListener('click', () =>{
+    overlayEnter.style.display = 'none'
+    overlayRegistration.style.display = 'flex'
+
+})
+
+overlayEnter.addEventListener('click', event => {
+    const target = event.target
+    if (target === overlayEnter || target.closest('.modal__close')) {
+        overlayEnter.style.display = 'none'
+        document.body.style.overflow = 'visible';
+        form.style.display = 'table'
+    }
+})
+
